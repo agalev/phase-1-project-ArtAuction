ee3f08e01766d800bb6939f3752f0728f06fae11
Alexander <alex.galev@yahoo.com>
2023-02-10 10:22:44 -0700

    looping through 12 objects and initilizing object for each one with data, then pushing it to locally stored array variable.

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 // Initialize pointers to DOM elements
 const container = document.querySelector('#container')
 
-// Fetch data from the Met Museum API and log it to the console
-// I found a bunch of good objects in sequence between 36500-36520+
+// Fetch data from the Met Museum API
+// Good objects in sequence between 10462-10474 or 11264-11276
+// We are using a 12 object range, which is a good sample data for the app
 // Change the number in the URL to see different objects
-fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/36520')
-	.then((response) => response.json())
-	.then((data) => {
-    console.log(data, data.primaryImage)
-		const image = document.createElement('img')
-		image.src = data.primaryImage
-		container.appendChild(image)
-	})
+
+// Initializing local state
+let localData = []
+
+for (let index = 11264; index < 11276; index++) {
+	fetch(
+		`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			const Object = {
+				title: data.title,
+				artist: data.artistDisplayName,
+				artistBio: data.artistDisplayBio,
+				image: data.primaryImage,
+				medium: data.medium,
+				date: data.objectDate,
+				credits: data.creditLine,
+				dimensions: data.dimensions,
+				repository: data.repository,
+				department: data.department,
+				development: data.objectEndDate - data.objectBeginDate
+			}
+			localData.push(Object)
+			const image = document.createElement('img')
+			image.src = data.primaryImage
+			image.class = 'img-thumbnail'
+			container.appendChild(image)
+		})
+}
+console.log(localData)
