e6eb74bf0af96bf04656ed3fb8f98ef37fce5bb1
Alexander <alex.galev@yahoo.com>
2023-02-10 14:35:59 -0700

    card work

--- index.js
+++ index.js
@@ -26,13 +26,47 @@ for (let index = 11264; index < 11276; index++) {
 				dimensions: data.dimensions,
 				repository: data.repository,
 				department: data.department,
-				development: data.objectEndDate - data.objectBeginDate
+				development: data.objectEndDate - data.objectBeginDate,
+				buyout: Math.floor(Math.random() * 10000000)
 			}
+			card(Object)
 			localData.push(Object)
-			const image = document.createElement('img')
-			image.src = data.primaryImage
-			image.class = 'img-thumbnail'
-			container.appendChild(image)
 		})
 }
-console.log(localData)
+
+const card = (piece) => {
+	const card = document.createElement('article')
+	card.addEventListener('click', () => {
+		expandedView(piece)
+	})
+	card.innerHTML = `
+		<div class="card h-100 w-75 mt-2">
+  		<img src="${piece.image}" class="card-img" alt="${piece.title}">
+			<span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+				Buyout: $${piece.buyout.toLocaleString('en-US')}</span>
+  		<div class="card-body">
+    		<h5 class="card-title">${piece.title}</h5>
+    		<p class="card-text">Artist: ${piece.artistBio}</p>
+  		</div>
+  		<ul class="list-group list-group-flush">
+				<li class="list-group-item">Medium: ${piece.medium}</li>
+				<li class="list-group-item">Dimensions: ${piece.dimensions}</li>
+				<li class="list-group-item">Completed: ${piece.date}</li>
+  		</ul>
+  		<div class="card-body">
+				<div class="input-group mb-3">
+					<span class="input-group-text">$</span>
+					<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+					<span class="input-group-text">.00</span>
+					<button class="btn btn-outline-success" type="button">Bid</button>
+				</div>
+  		</div>
+		</div>
+`
+	container.append(card)
+}
+
+const expandedView = (piece) => {
+	console.log(piece)
+	// 	container.innerHTML = ''
+}
